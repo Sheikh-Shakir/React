8764d11b4e9b188cee2f222f8badad4784d8d4c4
ShakirAhmad <shakirmca16@gmail.com>
2022-12-18 22:29:09 +0530

    React class based Components

--- React-06/src/App.js
+++ React-06/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-
+import React, { useState, useCallback } from 'react';
+import Button from './components/UI/Button/Button';
 import './App.css';
-
+import DemoOutput from './components/Demo/DemoOutput';
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setallowToggle] = useState(false);
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setallowToggle(true)
+  }
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      {/* <p>hi There !</p>  */}
+
+      <Button onClick={allowToggleHandler}>Alow Toggle</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
