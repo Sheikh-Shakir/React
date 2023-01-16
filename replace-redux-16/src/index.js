421b6724f58f6a86803c150f21d54cec334bd705
ShakirAhmad <shakirmca16@gmail.com>
2023-01-16 11:30:42 +0530

    react hooks

--- replace-redux-16/src/index.js
+++ replace-redux-16/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import ProductsProvider from './context/products-contexts';
+import configureProductsStore from './hooks-store/products-store';
+
+configureProductsStore();
 
 ReactDOM.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
