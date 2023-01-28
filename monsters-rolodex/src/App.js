9243dd4013c14fda3239ec43b97aefc0888939dc
ShakirAhmad <shakirmca16@gmail.com>
2023-01-28 21:14:50 +0530

    Monster rolodex app

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
-
+import { SearchBox } from './search-box/search-box.component';
 class App extends Component {
   constructor() {
     super();
@@ -9,21 +9,31 @@ class App extends Component {
       monsters: [],
       searchField: ''
     };
+
   };
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
   }
+  handleChange = (event) => {
+    this.setState({ searchField: event.target.value })
+  }
+
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
     return (
       <div className='app'>
-        <input type='search' placeholder='Search Monoster' onChange={e => {
-          this.setState({ searchField: e.target.value },);
-
-        }}
+        <h1>Monstors Rolodex</h1>
+        <SearchBox
+          placeholder='Search Monster'
+          handleChange={this.handleChange}
         />
-        <CardList monsters={this.state.monsters} />
+
+        <CardList monsters={filteredMonsters} />
 
       </div>
     );
